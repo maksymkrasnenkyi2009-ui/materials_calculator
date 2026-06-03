b9a076f3d8f22749bcda1ee1beb3f81c341c00ee
MaksymK <maksymkrasnenkyi2009@gmail.com>
2026-06-03 13:47:55 +0100

    Configure app to run on specified port

--- app.py
+++ app.py
@@ -61,4 +61,6 @@ def calculate():
     return jsonify({'sheets': sheets})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
